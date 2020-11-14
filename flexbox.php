d5b07d5134a204004bf99d59c8bf8ced1397ebc1
luis.seidel <luis.guilherme.seidel@gmail.com>
2020-11-14 17:49:53 -0300

    #icons renew

---
+++ flexbox.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste flexbox</title>
+    <link rel="stylesheet" href="/css/reset.css">
+    <style>
+
+    </style>
+</head>
+<body>
+    
+    <section class="container">
+
+        <article class="card">
+            <div class="card-image">
+                <img src="/images/webpath-logo.png" alt="Logo">
+            </div>
+            <div class="card-contend">
+                <h2>Titulo</h2>
+                <p>Esse é um resumo do que tem no card. Ele deve ser curto e ocupar no máximo 2 linhas</p>
+                <div class="card-links">
+                    <img src="/icons/business/png/001-dollar.png">
+                </div>
+            </div>
+        </article>
+
+    </section>
+
+</body>
+</html>
