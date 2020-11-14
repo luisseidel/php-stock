c077326643f8630ae7d1efc777cc2ed4ef9c4aac
luis.seidel <luis.guilherme.seidel@gmail.com>
2020-11-14 18:06:20 -0300

    #icons working

--- flexbox.php
+++ flexbox.php
@@ -6,7 +6,15 @@
     <title>Teste flexbox</title>
     <link rel="stylesheet" href="/css/reset.css">
     <style>
+        .icon {
+            max-width: 30px;
+            max-height: 30px;
+        }
 
+        .flex-row { display: flex; flex-direction: row; }
+        .flex-column { display: flex; flex-direction: column; }
+        .flex-row-reverse { display: flex; flex-direction: row-reverse; }
+        .flex-column-reverse { display: flex; flex-direction: column-reverse; }
     </style>
 </head>
 <body>
@@ -21,7 +29,8 @@
                 <h2>Titulo</h2>
                 <p>Esse é um resumo do que tem no card. Ele deve ser curto e ocupar no máximo 2 linhas</p>
                 <div class="card-links">
-                    <img src="/icons/business/png/001-dollar.png">
+                    <img class="icon" src="/images/interface/svg/share-light.svg" alt="">                    
+                    <img class="icon" src="/images/social-rounded/png/facebook.png" alt="">
                 </div>
             </div>
         </article>
